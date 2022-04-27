30e5cc1cb5599645f2fbe7cdf5f4fb5d75c9ab5d
aditya-K2 <adityakurdunkar2@gmail.com>
2022-04-27 09:10:08 +0530

    Initial Commit

---
+++ twitterserver/database.py
@@ -0,0 +1,230 @@
+import mysql.connector
+import os
+import uuid
+import bcrypt
+# import json
+
+GET_TIME_LINE_STATEMENT = '''( SELECT user_id, tweet_id, NULL as subtweet_id, tweet_body, time_posted FROM Tweets WHERE user_id in ( SELECT follows FROM followers WHERE user_id = "{user_id}" ))
+                            UNION ( SELECT user_id, retweet_id, subtweet_id, retweet_body, time_posted FROM Retweets WHERE user_id in ( SELECT follows FROM followers WHERE user_id = "{user_id}" ))
+                            UNION (( SELECT user_id, tweet_id, NULL as subtweet_id, tweet_body, time_posted FROM Tweets WHERE user_id = "{user_id}" )
+                                    UNION ( SELECT user_id, retweet_id, subtweet_id, retweet_body, time_posted FROM Retweets WHERE user_id = "{user_id}" ))
+                            ORDER BY time_posted DESC'''
+GET_USER_TIME_LINE = '''( SELECT user_id, tweet_id, NULL as subtweet_id, tweet_body, time_posted FROM Tweets WHERE user_id = "{user_id}" )
+                        UNION ( SELECT user_id, retweet_id, subtweet_id, retweet_body, time_posted FROM Retweets WHERE user_id = "{user_id}" ) ORDER BY time_posted DESC'''
+GET_TWEET_TIME_LINE = 'SELECT * FROM Comments WHERE parent_tweet = "{tweet_id}" ORDER BY time_posted DESC'
+GET_TWEET_STATMENT = '''( SELECT user_id, tweet_id, NULL as subtweet_id, tweet_body, time_posted FROM Tweets WHERE tweet_id = "{tweet_id}" )
+                        UNION ( SELECT user_id, retweet_id, subtweet_id, retweet_body, time_posted FROM Retweets WHERE retweet_id = "{tweet_id}" )
+                        UNION ( SELECT user_id, comment_id, NULL as subtweet_id, comment_body, time_posted FROM Comments WHERE comment_id = "{tweet_id}" ) ORDER BY time_posted DESC'''
+GET_SINGLE_TWEET_STATMENT = '''( SELECT user_id, tweet_id, tweet_body, time_posted FROM Tweets WHERE tweet_id = "{tweet_id}" )
+                        UNION ( SELECT user_id, retweet_id, retweet_body, time_posted FROM Retweets WHERE retweet_id = "{tweet_id}" )
+                        UNION ( SELECT user_id, comment_id, comment_body, time_posted FROM Comments WHERE comment_id = "{tweet_id}" ) ORDER BY time_posted DESC'''
+INSERT_TWEET_STATEMENT = 'INSERT INTO Tweets(user_id, tweet_id, tweet_body, time_posted) VALUES (%s,%s,%s, NOW())'
+INSERT_COMMENT_STATEMENT = 'INSERT INTO Comments(user_id, comment_id, parent_tweet, comment_body, time_posted) VALUES (%s,%s,%s, %s, NOW())'
+INSERT_RETWEET_STATEMENT = 'INSERT INTO Retweets(user_id, retweet_id, subtweet_id, retweet_body, time_posted) VALUES (%s,%s,%s, %s, NOW())'
+GET_USER_STATEMENT = 'SELECT user_id, password FROM Users WHERE user_id = "{user_id}"'
+
+NO_TWEET_ERROR = {
+    "error" : "No Tweet"
+}
+
+SUCCESS_STATUS = {
+    "status" : "successful addition of the user to database",
+}
+
+USER_ALREADY_EXISTS_ERROR = {
+    "error" : "User Already Exists"
+}
+
+db = mysql.connector.connect(
+        host=os.environ["TWITTER_DB_HOST"],
+        user=os.environ["TWITTER_DB_USER"],
+        passwd=os.environ["TWITTER_DB_PASSWORD"],
+        database=os.environ["TWITTER_DB_NAME"])
+
+def get_follow_relation(user_id, fo_id):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute('SELECT * from followers where user_id = "{user_id}" AND follows = "{fo_id}"'.format(user_id = user_id, fo_id = fo_id))
+    res = cursor.fetchone()
+    if res == None:
+        return "Follow"
+    else:
+        return "Unfollow"
+
+def follow(user_id, fo_id):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    if get_follow_relation(user_id, fo_id) == "Follow":
+        val = (user_id, fo_id)
+        cursor.execute('INSERT INTO followers(user_id, follows) VALUES(%s,%s)', val)
+        db.commit()
+        cursor.close()
+    else:
+        val = (user_id, fo_id)
+        cursor.execute('DELETE FROM followers WHERE user_id="{user_id}" AND follows="{fo_id}"'.format(user_id = user_id, fo_id = fo_id))
+        db.commit()
+        cursor.close()
+
+def insert_retweet(user_id, retweet_body, subtweet_id):
+       tweet_id = uuid.uuid1()
+       db.reconnect()
+       cursor = db.cursor(buffered=True)
+       val = (user_id, str(tweet_id), subtweet_id, retweet_body)
+       cursor.execute(INSERT_RETWEET_STATEMENT, val)
+       db.commit()
+       cursor.close()
+
+def check_user(user_id, passwd):
+       db.reconnect()
+       cursor = db.cursor(buffered=True)
+       cursor.execute(GET_USER_STATEMENT.format(user_id = user_id))
+       res = cursor.fetchone()
+       if res == None:
+           return False
+       else:
+            if bcrypt.checkpw(passwd.encode("utf-8"), res[1].encode("utf-8")):
+                return True
+            else:
+                return False
+
+def signin(user_id, passwd):
+       hash = bcrypt.hashpw(passwd.encode("utf-8"), bcrypt.gensalt())
+       db.reconnect()
+       cursor = db.cursor(buffered=True)
+       cursor.execute(GET_USER_STATEMENT.format(user_id = user_id))
+       res = cursor.fetchone()
+       if res == None:
+           val = (user_id, hash)
+           cursor.execute('INSERT INTO Users(user_id, password) VALUES(%s, %s)', val)
+           cursor.close()
+           db.commit()
+           return True
+       else:
+           return False
+
+def insert_comment(user_id, cb, parent_tweet_id):
+       tweet_id = uuid.uuid1()
+       db.reconnect()
+       cursor = db.cursor(buffered=True)
+       val = (user_id, str(tweet_id), parent_tweet_id, cb)
+       cursor.execute(INSERT_COMMENT_STATEMENT, val)
+       db.commit()
+       cursor.close()
+
+def insert_tweet(user_id: str, tweet_body: str):
+       tweet_id = uuid.uuid1()
+       db.reconnect()
+       cursor = db.cursor(buffered=True)
+       val = (user_id, str(tweet_id), tweet_body)
+       cursor.execute(INSERT_TWEET_STATEMENT, val)
+       db.commit()
+       cursor.close()
+
+def get_tweet(tweet_id: str):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute(GET_TWEET_STATMENT.format(tweet_id= tweet_id))
+
+    res = cursor.fetchone()
+
+    if res != None:
+        if res[2] == None:
+            return {
+                "user_id" : res[0],
+                "tweet_id" : res[1],
+                "tweet_body" : res[3],
+                "time_posted" : res[4].strftime("%m/%d/%Y, %H:%M:%S")
+            }
+        else:
+            return {
+                "user_id" : res[0],
+                "tweet_id" : res[1],
+                "subtweet_id" : get_single_tweet(res[2]),
+                "tweet_body" : res[3],
+                "time_posted" : res[4].strftime("%m/%d/%Y, %H:%M:%S")
+            }
+
+    else:
+        return NO_TWEET_ERROR
+
+def get_single_tweet(tweet_id):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute(GET_SINGLE_TWEET_STATMENT.format(tweet_id= tweet_id))
+
+    res = cursor.fetchone()
+
+    if res != None:
+        return {
+            "user_id" : res[0],
+            "tweet_id" : res[1],
+            "tweet_body" : res[2],
+            "time_posted" : res[3].strftime("%m/%d/%Y, %H:%M:%S")
+        }
+    else:
+        return NO_TWEET_ERROR
+
+def get_user_time_line(user_id : str):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute(GET_USER_TIME_LINE.format(user_id= user_id))
+
+    payload = []
+    for tuple in cursor.fetchall():
+        tweet = {}
+        tweet["user_id"] = tuple[0]
+        tweet["tweet_id"] = tuple[1]
+        if tuple[2] != None:
+            st = get_tweet(tuple[2])
+            if st != NO_TWEET_ERROR:
+                tweet["subtweet_id"] = st
+        tweet["tweet_body"] = tuple[3]
+        tweet["time_posted"] = tuple[4].strftime("%m/%d/%Y, %H:%M:%S")
+        payload.append(tweet)
+
+    return payload
+
+def get_home_time_line(user_id : str):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute(GET_TIME_LINE_STATEMENT.format(user_id= user_id))
+
+    payload = []
+    for tuple in cursor.fetchall():
+        tweet = {}
+        tweet["user_id"] = tuple[0]
+        tweet["tweet_id"] = tuple[1]
+        if tuple[2] != None:
+            st = get_tweet(tuple[2])
+            if st != NO_TWEET_ERROR:
+                tweet["subtweet_id"] = st
+        tweet["tweet_body"] = tuple[3]
+        tweet["time_posted"] = tuple[4].strftime("%m/%d/%Y, %H:%M:%S")
+        payload.append(tweet)
+
+    return payload
+
+def get_tweet_time_line(tweet_id: str):
+    db.reconnect()
+    cursor = db.cursor(buffered=True)
+    cursor.execute(GET_TWEET_TIME_LINE.format(tweet_id= tweet_id))
+
+    payload = {}
+    parent_tweet = get_tweet(tweet_id)
+    if parent_tweet != NO_TWEET_ERROR:
+        payload["tweet"] = parent_tweet
+    else: return NO_TWEET_ERROR
+    payload["comments"] = []
+    for tuple in cursor.fetchall():
+        comment = {}
+        comment["comment_id"] = tuple[0]
+        comment["parent_tweet_id"] = tuple[1]
+        comment["user_id"] = tuple[2]
+        comment["comment_body"] = tuple[3]
+        comment["time_posted"] = tuple[4].strftime("%m/%d/%Y, %H:%M:%S")
+        payload["comments"].append(comment)
+
+    print(payload)
+    return payload
+
+get_tweet_time_line("b62fa5c0-c4f6-11ec-b539-a4c3f0c02461")
---
+++ twitterserver/urls.py
@@ -0,0 +1,147 @@
+"""twitterserver URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import json
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+from . import database
+import datetime
+import jwt
+
+SECRET_JWT = "GROUP_7"
+def home(request):
+    if 'JWT_KEY' in request.COOKIES:
+        try:
+            data = jwt.decode(request.COOKIES["JWT_KEY"], SECRET_JWT, algorithms=["HS256"])
+        except:
+            return render(request, "login.html")
+        context = { "user_id" : data["user_id"],
+                    "posts" : database.get_home_time_line(data["user_id"]),
+                    "upper_bar_title" : "Home"}
+        return render(request, 'home.html', context)
+    else:
+        return render(request, "login.html")
+
+def user(request, user_id):
+    if 'JWT_KEY' in request.COOKIES:
+        try:
+            data = jwt.decode(request.COOKIES["JWT_KEY"], SECRET_JWT, algorithms=["HS256"])
+        except:
+            return render(request, "login.html")
+        context = { "user_id" : data["user_id"],
+                    "posts" : database.get_user_time_line(user_id),
+                    "upper_bar_title" : user_id}
+        if data["user_id"] != user_id:
+            context["notself"] = "True"
+            context["nuser"] = user_id
+            context["followstatus"] = database.get_follow_relation(data["user_id"], user_id)
+        return render(request, 'home.html', context)
+    else:
+        return render(request, "login.html")
+
+def create_tweet(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    database.insert_tweet(body_data["user_id"], body_data["tweet_body"])
+    return HttpResponse()
+
+def tweet(request, tweet_id):
+    if 'JWT_KEY' in request.COOKIES:
+        try:
+            data = jwt.decode(request.COOKIES["JWT_KEY"], SECRET_JWT, algorithms=["HS256"])
+        except:
+            return render(request, "login.html")
+        result = database.get_tweet_time_line(tweet_id)
+        context = { "user_id" : data["user_id"],
+                    "comments" : ( result["comments"] if "comments" in result else []),
+                    "tweet" : result["tweet"],
+                    "upper_bar_title" : 'Tweet'}
+        return render(request, 'tweet.html', context)
+    else:
+        return render(request, "login.html")
+
+def retweet_render(request, subtweet_id):
+    if 'JWT_KEY' in request.COOKIES:
+        try:
+            data = jwt.decode(request.COOKIES["JWT_KEY"], SECRET_JWT, algorithms=["HS256"])
+        except:
+            return render(request, "login.html")
+        context = { "user_id" : data["user_id"],
+                    "subtweet_id" : subtweet_id,
+                    "upper_bar_title" : 'Retweet'}
+        return render(request, 'retweet.html', context)
+    else:
+        return render(request, "login.html")
+
+def retweet(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    database.insert_retweet(body_data["user_id"], body_data["retweet_body"], body_data["subtweet_id"])
+    return HttpResponse()
+
+def comment_on_tweet(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    database.insert_comment(body_data["user_id"], body_data["comment_body"], body_data["parent_tweet"])
+    return HttpResponse()
+
+def login(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    response = HttpResponse()
+    if database.check_user(body_data["user_id"], body_data["password"]):
+        jwt_key = jwt.encode({ "user_id" : body_data["user_id"], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, SECRET_JWT, )
+        response.set_cookie("JWT_KEY", jwt_key)
+        response.status_code = 200
+    else:
+        response.status_code = 400
+    return response
+
+def signin(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    response = HttpResponse()
+    if database.signin(body_data["user_id"], body_data["password"]):
+        jwt_key = jwt.encode({ "user_id" : body_data["user_id"], 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, SECRET_JWT, )
+        response.set_cookie("JWT_KEY", jwt_key)
+        response.status_code = 200
+    else:
+        response.status_code = 400
+    return response
+
+def follow(request):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    database.follow(body_data["user_id"], body_data["follows"])
+    return HttpResponse()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', home),
+    path('user/<str:user_id>', user, name="user_timeline"),
+    path('create_tweet/', create_tweet),
+    path('tweet/<str:tweet_id>', tweet, name="tweet_timeline"),
+    path('comment_on_tweet/', comment_on_tweet),
+    path('login/', login),
+    path('signin/', signin),
+    path('retweet/<str:subtweet_id>', retweet_render, name="retweet_render"),
+    path('retweet/', retweet, name="retweet"),
+    path('follow/', follow)
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
